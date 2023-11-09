de2f238e6c96e56e6c3818fd0291ca97882d029a
Chaela Dunston <87950974+Echinodermatagirl@users.noreply.github.com>
2023-11-09 14:20:42 -0500

    Update HCIFinalProject.py

--- HCIFinalProject.py
+++ HCIFinalProject.py
@@ -9,9 +9,11 @@ st.title("Brought to you by Group 3")
 #st.sidebar.text_input("YOUR NAME: ")
 st.text_input("YOUR NAME: ")
 conv_type = st.selectbox("Select your Zodiac Sign: ", options=["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"])
+conv_type = st.selectbox("Select your Gender: ", options=["Female", "Male", "Non-Binary"])
 
 st.text_input("PARTNER'S NAME: ")
 conv_type = st.selectbox("Select your Zodiac Sign: ", options= ["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"])
+conv_type = st.selectbox("Select your Gender: ", options=["Female", "Male", "Non-Binary"])
 
 #results = "Chances "
 st.sidebar.text_area("Enter your comments here")
