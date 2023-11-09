98a6a6047ffe8ada63bf88815f00cb1990acb53c
Chaela Dunston <87950974+Echinodermatagirl@users.noreply.github.com>
2023-11-09 14:50:35 -0500

    Update HCIFinalProject.py

--- HCIFinalProject.py
+++ HCIFinalProject.py
@@ -6,15 +6,15 @@ st.header("Love Zodiac Compatibility")
 st.title("Brought to you by Group 3")
 # st.sidebar.title("Brought to you by Group 3")
 
-# :::::::5 different widgets :::::::
+# ::::::: 5 different widgets :::::::
 # Text Input 
 st.text_input("YOUR NAME: ")
 # Selectbox 
 conv_type = st.selectbox("Select your Zodiac Sign: ", options=["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"])
 conv_type = st.selectbox("Select your Gender: ", options=["Female", "Male", "Non-Binary"])
-# Select-Slider 
+# Slider 
 age = st.slider('How old are you?', 0, 130, 25)
-st.write("I'm ", age, 'years old')
+st.write("You're ", age, 'years old')
 # Radio Button
 # 
 
