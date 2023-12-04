0460532587bcaced158cf3d760c064769caa7fb3
Gcast201 <150385087+Gcast201@users.noreply.github.com>
2023-12-03 20:54:53 -0500

    Update HCIFinalProject.py
    added "" so the option boxes would be empty

--- HCIFinalProject.py
+++ HCIFinalProject.py
@@ -48,9 +48,7 @@ st.markdown(page_color, unsafe_allow_html=True)
 
 st.sidebar.title("Any thoughts?")
 st.sidebar.header("We want to hear from you!")
-#if st.sidebar.selectbox("Email"):
-#    enter_email = st.text_input("Please enter your email we can contact you at!")
-#side bar
+#contact options
 option = st.sidebar.selectbox("How do you prefer to be contacted?", options=[
     "", "Email", "Phone Call", "Text Message", "I prefer not to be contacted."])
 
@@ -75,15 +73,15 @@ if option and agree_terms:
 
 # User Input for User
 user_name = st.text_input("YOUR NAME:")
-user_zodiac = st.selectbox("Select your Zodiac Sign☽:", options=["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"])
-user_gender = st.selectbox("Select your Gender:", options=["Female", "Male", "Non-Binary"])
+user_zodiac = st.selectbox("Select your Zodiac Sign☽:", options=["","Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"])
+user_gender = st.selectbox("Select your Gender:", options=["", "Female", "Male", "Non-Binary"])
 user_age = st.slider('How old are you?', 0, 130, 25)
 st.write("You're", user_age, 'years old')
 
 # User Input for Partner
 partner_name = st.text_input("PARTNER'S NAME:")
-partner_zodiac = st.selectbox("Select your Partner's Zodiac Sign☽:", options=["Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"])
-partner_gender = st.selectbox("Select your Partner's Gender:", options=["Female", "Male", "Non-Binary"])
+partner_zodiac = st.selectbox("Select your Partner's Zodiac Sign☽:", options=["","Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"])
+partner_gender = st.selectbox("Select your Partner's Gender:", options=["","Female", "Male", "Non-Binary"])
 partner_age = st.slider("How old is your partner?", 0, 130, 25)
 st.write("Your partner is", partner_age, "years old")
 
