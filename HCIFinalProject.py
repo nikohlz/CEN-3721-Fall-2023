5e8f46c33829270f4e639a5e6ee0751a7dbefb30
Chaela Dunston <87950974+Echinodermatagirl@users.noreply.github.com>
2023-12-03 19:03:37 -0500

    Update HCIFinalProject.py

--- HCIFinalProject.py
+++ HCIFinalProject.py
@@ -66,8 +66,33 @@ partner_gender = st.selectbox("Select your Partner's Gender:", options=["Female"
 partner_age = st.slider("How old is your partner?", 0, 130, 25)
 st.write("Your partner is", partner_age, "years old")
 
+def generate_compatibility_data():
+    zodiac_signs = [
+        "Capricorn", "Aquarius", "Pisces", "Aries", "Taurus", "Gemini",
+        "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius"
+    ]
+    compatibility_data = np.random.rand(len(zodiac_signs), len(zodiac_signs)) * 25
+    return compatibility_data, zodiac_signs
+
+
 if st.button("Submit"):
     if user_name and user_zodiac and user_gender and partner_name and partner_zodiac and partner_gender:
         st.success("You’ve successfully submitted your info, let’s see your matches! 💟")
+        compatibility_matrix, signs = generate_compatibility_data()
+
+        # Filter compatibility scores for selected user and partner signs
+        user_index = signs.index(user_zodiac)
+        partner_index = signs.index(partner_zodiac)
+        user_compat_scores = compatibility_matrix[user_index]
+        partner_compat_scores = compatibility_matrix[:, partner_index]
+
+        # Create DataFrame for the selected compatibility scores
+        chart_data = pd.DataFrame({
+            'Your Compatibility': user_compat_scores,
+            "Partner's Compatibility": partner_compat_scores
+        }, index=signs)
+
+        # Display the bar chart for the selected compatibility scores
+        st.bar_chart(chart_data)
     else:
         st.error("Please fill out all the fields. 🚨")
